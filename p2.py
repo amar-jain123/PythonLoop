e65b0d00b2570090bd432e1067058572e2caf7ab
amarjain <amarjain>
2020-03-22 01:07:56 +0530

    added negative indexing

--- p2.py
+++ p2.py
@@ -1,3 +1,9 @@
 # use of range
 for i in range(0,10):
     print(i)
+
+# negative indexing in python
+list1 = [1,2,3,4,5]
+
+# suppose we want to access last element of list
+print('Last element of list1', list1[-1])
